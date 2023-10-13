ff9a0922cb69ff591eea88e04f1a574c82e03f23
Steax <127614802+iSteax@users.noreply.github.com>
2023-10-13 15:44:52 -0300

    Update notion_api_not_using.py
    delete IDs

--- notion_api_not_using.py
+++ notion_api_not_using.py
@@ -3,11 +3,11 @@ import json
 
 
 # Your Notion API key and database ID
-API_KEY = 'secret_T5YjPnpQAocyzKzVTr5TXk1mg30CXs2I4KtN5xUlVsy'
-DATABASE_ID = 'dd9097c4-ba75-41a5-87e9-ddfab6fe8b14'
+API_KEY = '****'
+DATABASE_ID = '****'
 
 # Endpoint URL
-url = 'https://api.notion.com/v1/databases/dd9097c4-ba75-41a5-87e9-ddfab6fe8b14/query'
+url = 'https://api.notion.com/v1/databases/****/query'
 
 # Headers
 headers = {
