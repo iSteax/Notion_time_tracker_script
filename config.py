d015116f7839745d273e4156e01e28eb30b9ae7e
Steax <127614802+iSteax@users.noreply.github.com>
2023-10-13 15:12:50 -0300

    Update config.py
    deleting ID's

--- config.py
+++ config.py
@@ -3,11 +3,8 @@ from notion_client import Client
 
 # Initialize Notion client
 TOKEN = {
-    "secret_T5YjPnpQAocyzKzVTr5TXk1mg30CXs2I4KtN5xUlVsy":"token_steaxtr@gmail.com",
+    "***",
 }
 database_id = {
-    # "dd9097c4-ba75-41a5-87e9-ddfab6fe8b14" :'Tasks_AdminOS' ,
-    # "2cfd511a4a9d413486280883648084c9":'Tasks_time_tracker' ,
-    # "31c2898f3108433e87b158b75b2b5c1e":'Tasks_test DB_deploied',
-    "fd029b9df843441cac2b6dd6f3d57228": 'Tasks_time_tracker_test_DB_local_code',
+    "***": 'Tasks_time_tracker_test_DB_local_code',
 }
